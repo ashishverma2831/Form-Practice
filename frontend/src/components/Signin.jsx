29fb1f82a24b6fc4ac28859ce261b61a2916435d
ashishverma2831 <ashishverma510207@gmail.com>
2024-01-07 02:31:19 +0530

    Update Signin and Signup components to use Formik

--- frontend/src/components/Signin.jsx
+++ frontend/src/components/Signin.jsx
@@ -1,24 +1,27 @@
+import { useFormik } from 'formik'
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const Signin = () => {
+
+  const loginForm = useFormik({
+    initialValues: { email: '', password: '' },
+    onSubmit:(values)=>{
+      console.log(values);
+    }
+  })
+
   return (
     <>
         <section className="bg-gray-50 dark:bg-gray-900">
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-    <a
-      href="#"
-      className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-    >
-      
-      Login
-    </a>
+    
     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
       <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+        <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
           Sign in to your account
         </h1>
-        <form className="space-y-4 md:space-y-6" action="#">
+        <form className="space-y-4 md:space-y-6" onSubmit={loginForm.handleSubmit}>
           <div>
             <label
               htmlFor="email"
@@ -31,8 +34,10 @@ const Signin = () => {
               name="email"
               id="email"
               className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="name@company.com"
+              placeholder="mukesh@gmail.com"
               required=""
+              onChange={loginForm.handleChange}
+              value={loginForm.values.email}
             />
           </div>
           <div>
@@ -49,6 +54,8 @@ const Signin = () => {
               placeholder="••••••••"
               className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               required=""
+              onChange={loginForm.handleChange}
+              value={loginForm.values.password}
             />
           </div>
           {/* <div className="flex items-center justify-between">
