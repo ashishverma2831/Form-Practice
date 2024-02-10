41e36eb6810040df8db79a96800484734e37dac8
ashishverma2831 <ashishverma510207@gmail.com>
2024-02-10 22:57:37 +0530

    Add file upload functionality to App.jsx and Header.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes,Route} from 'react-router-dom'
 import Signin from './components/Signin'
 import Signup from './components/Signup'
 import {SnackbarProvider} from 'notistack'
+import FileUpload from './components/FileUpload'
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
           <Route path="/" element={<Header />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/fileupload" element={<FileUpload />} />
         </Routes>
       </BrowserRouter>
       </SnackbarProvider>
---
+++ frontend/src/components/FileUpload.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header'
+
+const FileUpload = () => {
+  return (
+    <>
+        <Header />
+        <section>
+            
+        </section>
+    </>
+  )
+}
+
+export default FileUpload
