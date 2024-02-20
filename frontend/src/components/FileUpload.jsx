7a215857fceff1bbd73f45d3cbfd9b37ad8aa23b
ashishverma2831 <ashishverma510207@gmail.com>
2024-02-20 12:45:21 +0530

    Add useState hook to FileUpload component

--- frontend/src/components/FileUpload.jsx
+++ frontend/src/components/FileUpload.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 
 const FileUpload = () => {
+  const [selFile, setSelFile] = useState('');
   const uploadFile = (e) => {
     const file = e.target.files[0];
     setSelFile(file.name);
